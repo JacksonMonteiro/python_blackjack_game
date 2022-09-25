73e69dee6c2aa7f142a1f18b4c3d4d27d94fd7ad
jacksonmonteiro <infor.jackson324@gmail.com>
2022-09-25 15:39:22 -0300

    Deck class

--- main.py
+++ main.py
@@ -1,42 +1,41 @@
 import random
 
-cards = []
-suits = ['Spades', 'Clubs', 'Hearts', 'Diamonds']
-ranks = [
-    {'rank': 'A', 'value': 11},
-    {'rank': '2', 'value': 2},
-    {'rank': '3', 'value': 3},
-    {'rank': '4', 'value': 4},
-    {'rank': '5', 'value': 5},
-    {'rank': '6', 'value': 6},
-    {'rank': '7', 'value': 7},
-    {'rank': '8', 'value': 8},
-    {'rank': '9', 'value': 9},
-    {'rank': '10', 'value': 10},
-    {'rank': 'J', 'value': 10},
-    {'rank': 'Q', 'value': 10},
-    {'rank': 'K', 'value': 10},
-]
+class Deck:
+    def __init__(self):
+        self.cards = []
+        suits = ['Spades', 'Clubs', 'Hearts', 'Diamonds']
+        ranks = [
+            {'rank': 'A', 'value': 11},
+            {'rank': '2', 'value': 2},
+            {'rank': '3', 'value': 3},
+            {'rank': '4', 'value': 4},
+            {'rank': '5', 'value': 5},
+            {'rank': '6', 'value': 6},
+            {'rank': '7', 'value': 7},
+            {'rank': '8', 'value': 8},
+            {'rank': '9', 'value': 9},
+            {'rank': '10', 'value': 10},
+            {'rank': 'J', 'value': 10},
+            {'rank': 'Q', 'value': 10},
+            {'rank': 'K', 'value': 10},
+        ]
 
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit, rank])
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append([suit, rank])
 
+    def shuffle(self):
+        if (len(self.cards) > 1):
+            random.shuffle(self.cards)
 
-def shuffle():
-    random.shuffle(cards)
+    def deal(self, number):
+        cards_dealt = []
+        for i in range(number):
+            if (len(self.cards) > 1):
+                cards_dealt.append(self.cards.pop())
+        return cards_dealt
 
+deck = Deck()
+deck.shuffle()
 
-def deal(number):
-    cards_dealt = []
-
-    for i in range(number):
-        cards_dealt.append(cards.pop())
-
-    return cards_dealt
-
-
-shuffle()
-
-card = deal(1)[0]
-print(card[1]['value'])
+print(deck.cards)
